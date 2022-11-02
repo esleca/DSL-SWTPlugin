873c2ee171a78dc1da2e8e753bdf30eec0688985
Karina Martinez Guerrero <karinamguerrero@estudiantec.cr>
2022-11-02 15:53:28 -0600

    Updated to use unittestresponse

--- src/main/java/com/dsl/plugin/commandApp/CommandAppApplication.java
+++ src/main/java/com/dsl/plugin/commandApp/CommandAppApplication.java
@@ -10,10 +10,11 @@ import com.dsl.factories.ValueTypeFactory;
 import com.dsl.models.dtos.ClassTestsRequest;
 import com.dsl.models.dtos.PackageTestsRequest;
 import com.dsl.models.dtos.UnitTestRequest;
+import com.dsl.models.dtos.UnitTestResponse;
 import com.dsl.models.unittests.UnitTest;
 import com.dsl.models.valuetypes.ValueType;
 import com.dsl.plugin.commandApp.Models.Message;
-import com.google.gson.Gson;
+import com.google.gson.*;
 import org.jetbrains.annotations.NotNull;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -33,6 +34,8 @@ public class CommandAppApplication implements CommandLineRunner {
     private static Logger LOG = LoggerFactory.getLogger(CommandAppApplication.class);
     private IDSLFachade dsl;
 
+    private ServerSocket serverSocket;
+
     private static final int portNumber = 3399;
 
     public CommandAppApplication(IDSLFachade dsl) {
@@ -51,98 +54,77 @@ public class CommandAppApplication implements CommandLineRunner {
      */
     @Override
     public void run(String... args) throws Exception {
-        ServerSocket serverSocket = new ServerSocket(portNumber);
-        Socket clientSocket = serverSocket.accept();
-        InputStreamReader input = new InputStreamReader(clientSocket.getInputStream());
-        OutputStreamWriter out = new OutputStreamWriter(clientSocket.getOutputStream());
 
-        Message msg = new Gson().fromJson(input, Message.class);
-
+        serverSocket = new ServerSocket(portNumber);
         JSONParser jsonParser = new JSONParser();
-
-        try  {
-            JSONObject request = (JSONObject) jsonParser.parse(msg.getRequest());
-            if (msg.getRequestCode().equals("LIST")) {
-                if(msg.getListCode().equals("CLASS")){
-                    LOG.info("EXECUTING : request to list unit test");
-                    String packageName = (String) request.get("packageName");
-                    String className = (String) request.get("className");
-
-                    ClassTestsRequest lstReq = new ClassTestsRequest(packageName, className);
-                    List<UnitTest> classList = dsl.getClassUnitTests(lstReq);
-                    List<String> strLst = new ArrayList<>();
-                    classList.forEach( element -> {
-                        strLst.add(element.toString());
-                    });
-                    out.write(new Gson().toJson(strLst).toString());
-                    //out.close();
-                }
-                else if(msg.getListCode().equals("PACKAGE")){
-                    LOG.info("EXECUTING : request to list unit test");
-                    String packageName = (String) request.get("packageName");
-
-                    PackageTestsRequest lstReq = new PackageTestsRequest(packageName);
-                    dsl.getPackageUnitTests(lstReq);
-                }
-            }
-            else {
-                String className = (String) request.get("className");
-                String assertion = (String) request.get("assertion");
-                String classPath = (String) request.get("classPath");
-                String language = (String) request.get("language");
-                String outputPath = (String) request.get("outputPath");
-                String function = (String) request.get("function");
-                String testName = (String) request.get("testName");
-                JSONArray parameters = (JSONArray) request.get("parameters");
-                JSONObject exp = (JSONObject) request.get("expected");
-                ValueType expected = ValueTypeFactory.createValueType(exp.get("type").toString(), exp.get("value").toString());
-                UnitTestRequest utRequest = new UnitTestRequest(classPath, outputPath, language, function, testName, parameters, expected, assertion);
-                if (msg.getRequestCode().equals("CREATE")) {
-                    LOG.info("EXECUTING : request to create unit test");
-                    dsl.generateUnitTest(utRequest);
-                } else if (msg.getRequestCode().equals("EDIT")) {
-                    LOG.info("EXECUTING : request to edit unit test");
-                    dsl.generateUnitTest(utRequest);
-                } else if (msg.getRequestCode().equals("DELETE")) {
-                    LOG.info("EXECUTING : request to delete unit test");
-                    dsl.removeUnitTest(utRequest);
-                }
-            }
-
-        } catch (ParseException e) {
-            System.err.println("Error reading the test scenarios config file.");
-            e.printStackTrace();
-        } catch (ValueTypeNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-       /* try {
-            ServerSocket serverSocket = new ServerSocket(portNumber);
+        while(true) {
             Socket clientSocket = serverSocket.accept();
-            InputStreamReader input = new InputStreamReader(clientSocket.getInputStream());
-            Message msg = new Gson().fromJson(input, Message.class);
-            UnitTestRequest request =  new Gson().fromJson(msg.getRequest(), UnitTestRequest.class);
-            LOG.info(msg.getRequest());
-            if (msg.getRequestCode().equals("CREATE")) {
-                LOG.info("EXECUTING : request to create unit test");
-                dsl.createUnitTest(request);
-            } else if (msg.getRequestCode().equals("EDIT")) {
-                LOG.info("EXECUTING : request to edit unit test");
-                dsl.editUnitTest(request);
-            } else if (msg.getRequestCode().equals("DELETE")) {
-                LOG.info("EXECUTING : request to delete unit test");
-                dsl.removeUnitTest(request);
-            } else if (msg.getRequestCode().equals("LIST")) {
-                LOG.info("EXECUTING : request to list unit test");
-                dsl.createUnitTest(request);
+
+            DataInputStream din=new DataInputStream(clientSocket.getInputStream());
+            DataOutputStream dout=new DataOutputStream(clientSocket.getOutputStream());
+            Message msg = new Gson().fromJson(din.readUTF(), Message.class);
+
+            /*InputStreamReader in = new InputStreamReader(clientSocket.getInputStream());
+            OutputStreamWriter out = new OutputStreamWriter(clientSocket.getOutputStream());
+            Message msg = new Gson().fromJson(in, Message.class);*/
+
+            //Message msg = gson.fromJson(din.readUTF(), Message.class);
+
+            try {
+                JSONObject request = (JSONObject) jsonParser.parse(msg.getRequest());
+                //Handling a request to list unit test at class or package level
+                if (msg.getRequestCode().equals("LIST")) {
+                    if (msg.getListCode().equals("CLASS")) {
+                        LOG.info("EXECUTING : request to list unit test");
+                        String packageName = (String) request.get("packageName");
+                        String className = (String) request.get("className");
+
+                        ClassTestsRequest lstReq = new ClassTestsRequest(packageName, className);
+                        List<UnitTestResponse> classList = dsl.getClassUnitTests(lstReq);
+                        dout.writeUTF(new Gson().toJson(classList).toString());
+                        dout.flush();
+
+                    } else if (msg.getListCode().equals("PACKAGE")) {
+                        LOG.info("EXECUTING : request to list unit test");
+                        String packageName = (String) request.get("packageName");
+
+                        PackageTestsRequest lstReq = new PackageTestsRequest(packageName);
+                        dsl.getPackageUnitTests(lstReq);
+                    }
+                } //Handling unit test creation, edition, or deletion
+                else {
+                    //Extract all the parameter necessary to create a unit test
+                    String className = (String) request.get("className");
+                    String assertion = (String) request.get("assertion");
+                    String classPath = (String) request.get("classPath");
+                    String language = (String) request.get("language");
+                    String outputPath = (String) request.get("outputPath");
+                    String function = (String) request.get("function");
+                    String testName = (String) request.get("testName");
+                    JSONArray parameters = (JSONArray) request.get("parameters");
+                    JSONObject exp = (JSONObject) request.get("expected");
+                    //Uses factory to obtain the correct value type
+                    ValueType expected = ValueTypeFactory.createValueType(exp.get("type").toString(), exp.get("value").toString());
+                    UnitTestRequest utRequest = new UnitTestRequest(classPath, outputPath, language, function, testName, parameters, expected, assertion);
+                    if (msg.getRequestCode().equals("CREATE")) {
+                        LOG.info("EXECUTING : request to create unit test");
+                        dsl.generateUnitTest(utRequest);
+                    } else if (msg.getRequestCode().equals("EDIT")) {
+                        LOG.info("EXECUTING : request to edit unit test");
+                        dsl.generateUnitTest(utRequest);
+                    } else if (msg.getRequestCode().equals("DELETE")) {
+                        LOG.info("EXECUTING : request to delete unit test");
+                        dsl.removeUnitTest(utRequest);
+                    }
+                }
+
+            } catch (ParseException e) {
+                System.err.println("Error reading the test scenarios config file.");
+                e.printStackTrace();
+            } catch (ValueTypeNotFoundException e) {
+                throw new RuntimeException(e);
             }
-            serverSocket.close();
-
-
-        } catch (Exception e) {
-            LOG.info("Something went wrong ¯\\_(ツ)_/¯");
-            LOG.info(e.getMessage());
-            e.printStackTrace();
-        }*/
+        }
     }
 
     public UnitTestRequest test() throws IOException {
